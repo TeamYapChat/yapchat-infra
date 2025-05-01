9d8c13ec372c0793ecfe26208be64ca195762775
KhiemNguyen15 <khiemsa15@gmail.com>
2025-05-01 13:00:41 -0500

    style: Reformat code with 'tofu fmt'

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "cdn_domain" {
-    description = "The CloudFront distribution domain name"
-    value       = aws_cloudfront_distribution.cdn.domain_name
+  description = "The CloudFront distribution domain name"
+  value       = aws_cloudfront_distribution.cdn.domain_name
 }
 
 output "bucket_name" {
-    description = "The name of the S3 bucket"
-    value       = aws_s3_bucket.cdn_bucket.id
+  description = "The name of the S3 bucket"
+  value       = aws_s3_bucket.cdn_bucket.id
 }
--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 variable "bucket_name" {
-    type        = string
-    description = "The name of the S3 bucket"
-    default     = "yapchat-files"
+  type        = string
+  description = "The name of the S3 bucket"
+  default     = "yapchat-files"
 }
 
 variable "region" {
-    type        = string
-    description = "AWS region"
-    default     = "us-east-1"
+  type        = string
+  description = "AWS region"
+  default     = "us-east-1"
 }
 
 variable "cdn_comment" {
-    type        = string
-    description = "A comment for the CloudFront distribution"
-    default     = "YapChat file CDN"
+  type        = string
+  description = "A comment for the CloudFront distribution"
+  default     = "YapChat file CDN"
 }
